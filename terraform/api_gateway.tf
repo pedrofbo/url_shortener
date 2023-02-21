c21d296d95c21c5a95e85ced9694c6ba9f39cbaf
pedrofbo <pedrofelipebo@gmail.com>
2023-02-21 15:46:31 -0300

    chore: import shorten endpoint resources into terraform

--- terraform/api_gateway.tf
+++ terraform/api_gateway.tf
@@ -35,9 +35,46 @@ resource "aws_api_gateway_integration" "create__post__lambda" {
   uri                     = aws_lambda_function.shorten.invoke_arn
 }
 
-resource "aws_lambda_permission" "api_gateway_invoke_lambda" {
+resource "aws_lambda_permission" "api_gateway_invoke_lambda__shorten" {
   function_name = aws_lambda_function.shorten.arn
   action        = "lambda:InvokeFunction"
   principal     = "apigateway.amazonaws.com"
   source_arn    = "arn:aws:execute-api:${var.region}:${local.account_id}:${aws_api_gateway_rest_api.url_shortener.id}/*/${aws_api_gateway_method.create__post.http_method}${aws_api_gateway_resource.create.path}"
+}
+
+###########################
+###  Redirect endpoint  ###
+###########################
+
+resource "aws_api_gateway_resource" "redirect" {
+  path_part   = "{short_url}"
+  rest_api_id = aws_api_gateway_rest_api.url_shortener.id
+  parent_id   = aws_api_gateway_rest_api.url_shortener.root_resource_id
+}
+
+resource "aws_api_gateway_method" "redirect__get" {
+  rest_api_id   = aws_api_gateway_rest_api.url_shortener.id
+  resource_id   = aws_api_gateway_resource.redirect.id
+  http_method          = "GET"
+  authorization        = "NONE"
+  request_parameters   = {
+    "method.request.path.short_url" = true
+  }
+}
+
+resource "aws_api_gateway_integration" "redirect__get__lambda" {
+  rest_api_id             = aws_api_gateway_rest_api.url_shortener.id
+  resource_id             = aws_api_gateway_resource.redirect.id
+  http_method             = "GET"
+  type                    = "AWS_PROXY"
+  integration_http_method = "POST"
+  content_handling        = "CONVERT_TO_TEXT"
+  uri                     = aws_lambda_function.redirect.invoke_arn
+}
+
+resource "aws_lambda_permission" "api_gateway_invoke_lambda__redirect" {
+  function_name = aws_lambda_function.redirect.arn
+  action        = "lambda:InvokeFunction"
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "arn:aws:execute-api:${var.region}:${local.account_id}:${aws_api_gateway_rest_api.url_shortener.id}/*/${aws_api_gateway_method.redirect__get.http_method}/*"
 }
